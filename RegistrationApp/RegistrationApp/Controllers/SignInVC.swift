852586c0727dd8f0d3967253db506656c49bec99
mkapanzhi <mak1917132@yandex.ru>
2022-08-03 00:50:33 +0300

    Created "Sign In" screen

---
+++ RegistrationApp/RegistrationApp/Controllers/SignInVC.swift
@@ -0,0 +1,32 @@
+//
+//  SignInVC.swift
+//  RegistrationApp
+//
+//  Created by Максим Капанжи on 3.08.22.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+    @IBOutlet var emeilTF: UITextField!
+    @IBOutlet var errorEmailLbl: UILabel!
+    @IBOutlet var passTF: UITextField!
+    @IBOutlet var errorPassLbl: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+     // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
